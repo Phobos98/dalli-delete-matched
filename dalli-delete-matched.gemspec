1935098d271a9fc336fe5a3019cc1d4dc7595e34
Phobos98 <ivan.kourza@perfectial.com>
2012-12-25 10:07:53 +0200

    Initial

---
+++ dalli-delete-matched.gemspec
@@ -0,0 +1,19 @@
+$:.push File.expand_path("../lib", __FILE__)
+
+
+# Describe your gem and declare its dependencies:
+Gem::Specification.new do |s|
+  s.name        = "dalli-delete-matched"
+  s.version     = "0.9.0"
+  s.authors     = ["Kourza Ivan a.k.a. Phobos98"]
+  s.email       = ["phobos98@phobos98.net"]
+  s.homepage    = "TODO"
+  s.summary     = ""
+  s.description = ""
+
+  s.files = Dir["{app,config,db,lib}/**/*"] + ["MIT-LICENSE", "Rakefile", "README.rdoc"]
+  s.test_files = Dir["test/**/*"]
+
+  s.add_dependency "dalli"
+
+end
---
+++ lib/dalli-delete-matched.rb
@@ -0,0 +1,51 @@
+ActiveSupport::Cache::DalliStore.class_eval do
+    
+  MEMCACHED_KEYS = "memcached_keys"
+  
+  alias_method :old_write_entry, :write_entry
+  def write_entry(key, entry, options)
+    keys = get_memcached_keys
+    unless keys.include?(key)
+      keys << key
+      return false unless old_write_entry(MEMCACHED_KEYS, keys.to_yaml, {})
+    end
+    old_write_entry(key, entry, options)
+  end
+  
+  alias_method :old_delete_entry, :delete_entry
+  def delete_entry(key, options)
+    ret = old_delete_entry(key, options)
+    return false unless ret
+    keys = get_memcached_keys
+    if keys.include?(key)
+      keys -= [ key ]
+      old_write_entry(MEMCACHED_KEYS, keys.to_yaml, {})
+    end
+    ret
+  end
+  
+  def delete_matched(matcher, options = nil)
+    ret = true
+    deleted_keys = []
+    keys = get_memcached_keys
+    keys.each do |key|
+      if ret && key.match(matcher)
+        deleted_keys << key if (ret = old_delete_entry(key, options))
+      end
+    end
+    len = keys.length
+    keys -= deleted_keys
+    old_write_entry(MEMCACHED_KEYS, keys.to_yaml, {}) if keys.length < len
+    ret
+  end
+
+private
+  def get_memcached_keys
+    begin
+      YAML.load read(MEMCACHED_KEYS)
+    rescue TypeError
+      []
+    end
+  end
+  
+end
