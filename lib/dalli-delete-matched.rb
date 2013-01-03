02dcd2c45bab6fc69894f01b049fd873fefc907f
Phobos98 <phobos98@phobos98.net>
2013-01-03 12:38:58 +0200

    Some vars renamed

--- lib/dalli-delete-matched.rb
+++ lib/dalli-delete-matched.rb
@@ -3,14 +3,14 @@ require 'active_support/core_ext/module/aliasing'
 
 ActiveSupport::Cache::DalliStore.class_eval do
     
-  MEMCACHED_KEYS = "memcached_keys"
+  CACHE_KEYS = "CacheKeys"
   
   alias_method :old_write_entry, :write_entry
   def write_entry(key, entry, options)
-    keys = get_memcached_keys
+    keys = get_cache_keys
     unless keys.include?(key)
       keys << key
-      return false unless old_write_entry(MEMCACHED_KEYS, keys.to_yaml, {})
+      return false unless old_write_entry(CACHE_KEYS, keys.to_yaml, {})
     end
     old_write_entry(key, entry, options)
   end
@@ -19,10 +19,10 @@ ActiveSupport::Cache::DalliStore.class_eval do
   def delete_entry(key, options)
     ret = old_delete_entry(key, options)
     return false unless ret
-    keys = get_memcached_keys
+    keys = get_cache_keys
     if keys.include?(key)
       keys -= [ key ]
-      old_write_entry(MEMCACHED_KEYS, keys.to_yaml, {})
+      old_write_entry(CACHE_KEYS, keys.to_yaml, {})
     end
     ret
   end
@@ -30,7 +30,7 @@ ActiveSupport::Cache::DalliStore.class_eval do
   def delete_matched(matcher, options = nil)
     ret = true
     deleted_keys = []
-    keys = get_memcached_keys
+    keys = get_cache_keys
     keys.each do |key|
       if ret && key.match(matcher)
         deleted_keys << key if (ret = old_delete_entry(key, options))
@@ -38,14 +38,14 @@ ActiveSupport::Cache::DalliStore.class_eval do
     end
     len = keys.length
     keys -= deleted_keys
-    old_write_entry(MEMCACHED_KEYS, keys.to_yaml, {}) if keys.length < len
+    old_write_entry(CACHE_KEYS, keys.to_yaml, {}) if keys.length < len
     ret
   end
 
 private
-  def get_memcached_keys
+  def get_cache_keys
     begin
-      YAML.load read(MEMCACHED_KEYS)
+      YAML.load read(CACHE_KEYS)
     rescue TypeError
       []
     end
