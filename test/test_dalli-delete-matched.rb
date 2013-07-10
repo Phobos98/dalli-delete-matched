42717743578a3fc54b435c5048904ebfacec1943
Erik Erwitt <eerwitt@gmail.com>
2013-07-10 12:45:05 -0700

    added the first test to be used with a mock memcache connections

--- test/test_dalli-delete-matched.rb
+++ test/test_dalli-delete-matched.rb
@@ -3,11 +3,15 @@ require 'helper'
 describe "DalliDeleteMatched" do
   describe "Cache Keys" do
     it "checks for cache keys" do
-      assert_equal ActiveSupport::Cache::DalliStore.new('localhost:19122').send(:get_cache_keys), []
+      memcached(19121, '', {}) do |dc|
+        assert_equal ActiveSupport::Cache::DalliStore.new('localhost:19121').send(:get_cache_keys), []
+      end
     end
 
     it "inserts a cache key and returns it as a key" do
       memcached(19122, '', {}) do |dc|
+        dc.flush
+
         store = ActiveSupport::Cache::DalliStore.new('localhost:19122')
         store.send :write_entry, "test", "content", {}
 
